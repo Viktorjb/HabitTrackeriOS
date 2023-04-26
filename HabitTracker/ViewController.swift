415861c1cdc09e7ca681f0ed5b614c44d41c95b1
Viktorjb <vjagebrant@gmail.com>
2023-04-26 22:49:32 +0200

    date handling

--- HabitTracker/ViewController.swift
+++ HabitTracker/ViewController.swift
@@ -13,7 +13,41 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy/MM/dd"
+        
+        let theDate = Date()
+        let dateString = dateFormatter.string(from: theDate)
+        print(dateString)
+        print(yesterday(day:dateString))
+        
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func yesterday(day: String) -> String{
+        let yearInt = Int(day.prefix(4)) ?? 11
+        let monthInt = Int(day.suffix(5).prefix(2)) ?? 11
+        let dayInt = Int(day.suffix(2)) ?? 11
+        
+        
+        //handle going back a day
+        
+        
+        let yearString = String(yearInt)
+        var monthString = String(monthInt)
+        if(monthString.count < 2){
+            monthString = "0" + monthString
+        }
+        var dayString = String(dayInt)
+        if(dayString.count < 2){
+            dayString = "0" + dayString
+        }
+            
+        let newString = yearString + "/" + monthString + "/" + dayString
+        
+        return newString
     }
 
 
